e636d79cc518f855d491ac733797345ac178d5a3
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-01-06 20:11:12 -0300

    feat: add initial add Product button

---
+++ app/products/_components/add-product-button.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import { z } from "zod";
+import { Loader2Icon, PlusIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/_components/ui/form";
+import { Input } from "@/app/_components/ui/input";
+import { NumericFormat } from "react-number-format";
+
+const AddProductButton = () => {
+  // Definição do schema de validação
+  const formSchema = z.object({
+    name: z.string().trim().min(1, {
+      message: "O nome do produto é obrigatório.",
+    }),
+    price: z.number().min(0.01, {
+      message: "O preço do produto deve ser maior que R$ 0,01.",
+    }),
+    stock: z.coerce.number().int().positive({
+      message: "A quantidade em estoque deve ser maior que zero.",
+    }),
+  });
+
+  type ProductSchema = z.infer<typeof formSchema>;
+
+  // Configuração do React Hook Form
+  const form = useForm<ProductSchema>({
+    shouldUnregister: true,
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      price: 0,
+      stock: 1,
+    },
+  });
+
+  // Função para tratar submissão do formulário
+  const onSubmit = (data: ProductSchema) => {
+    console.log({ data });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"default"} className="gap-2 self-end">
+          <PlusIcon size={20} />
+          Novo Produto
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <DialogHeader>
+              <DialogTitle>Criar Produto</DialogTitle>
+              <DialogDescription>
+                Insira as informações abaixo para cadastrar o produto.
+              </DialogDescription>
+            </DialogHeader>
+
+            {/* Campo Nome */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite o nome do produto" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Campo Preço */}
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Preço</FormLabel>
+                  <FormControl>
+                    <NumericFormat
+                      thousandSeparator="."
+                      decimalSeparator=","
+                      fixedDecimalScale
+                      decimalScale={2}
+                      prefix="R$ "
+                      allowNegative={false}
+                      customInput={Input}
+                      onValueChange={(values) =>
+                        field.onChange(values.floatValue)
+                      }
+                      value={field.value || ""}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Campo Estoque */}
+            <FormField
+              control={form.control}
+              name="stock"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Estoque</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Digite o estoque do produto"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="secondary" type="reset">
+                  Cancelar
+                </Button>
+              </DialogClose>
+              <Button
+                type="submit"
+                disabled={form.formState.isSubmitting}
+                className="gap-1.5"
+              >
+                {form.formState.isSubmitting && (
+                  <Loader2Icon className="animate-spin" size={16} />
+                )}
+                Salvar
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddProductButton;
