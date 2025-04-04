54a146c366ea1329bc63b2f5668d4de85c5ad18a
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-04-03 22:27:12 -0300

    feat: ensure that sales and products page are scrollable

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -26,7 +26,7 @@ const sales = async () => {
   }));
 
   return (
-    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+    <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
       <Header>
         <HeaderLeft>
           <HeaderSubtitle>Gestão de Vendas</HeaderSubtitle>
